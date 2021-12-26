aa07e5ac2777bf9a676f27cd6163f3842733c177
Ryan Bourdais <ryanbourdais@outlook.com>
2021-12-25 23:30:32 -0600

    completed up to module 309: Fetch Single Product

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk " />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline" />
+            <p>
+              Sunt duis Lorem enim eiusmod ipsum exercitation. Mollit dolore
+              mollit aliquip nulla nulla aliqua est officia laborum minim. Ex
+              tempor adipisicing esse nulla laborum ipsum commodo fugiat cillum
+              labore minim veniam sint. Veniam ullamco Lorem quis officia do
+              deserunt ullamco labore consectetur duis sint laboris eu. Ex duis
+              voluptate quis consectetur ex in qui ullamco ex occaecat non sunt.
+              Id aliqua deserunt laboris id ea pariatur officia reprehenderit eu
+              non qui laboris deserunt nostrud. Non pariatur occaecat id tempor
+              nostrud pariatur.
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +57,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
